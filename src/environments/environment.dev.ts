3f3722f1bb939d266cb951513a35cb4ae23cc44e
Suthida Sattayawong <ssuthida.w@gmail.com>
2022-10-01 01:00:29 +0700

    fix environment.dev

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -3,13 +3,20 @@ import { NgxLoggerLevel } from 'ngx-logger';
 export const environment = {
   production: false,
   env: 'dev',
-  Singularity: {
+  authen: {
+    domain: 'https://104.43.115.28:23020/auth/v3.1',
+    clientId: 'wO6kzH+uR2FMCbco8D4MACn6EJ1sRRFag7vkkVSBV+9CkFtdFVEZaA==',
+    clientSecret: 'clientSecret',
+    redirectUri: 'http://104.43.115.28:23001/home', 
+    beEndpoint: "http://104.43.115.28:23001/api/v1"
+  },
+  DigitalCo: {
     endpointApi: 'https://iot-authv3.ais.co.th',
     endpointWeb: 'https://iot-authv3.ais.co.th',
     Parameters: {
       identityHeader: 'X-Digitalco-Id'
     },
-    endpointApiCube:'https://stg-digital.ais.th/actionable-insight/api/analytic/v1'
+    endpointApiCube: 'http://localhost:4000/analytic/v1'
   },
   Logger: {
     logLevel: NgxLoggerLevel.TRACE,
