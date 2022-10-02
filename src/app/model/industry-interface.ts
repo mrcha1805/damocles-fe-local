6a95aa5eb12a9911d46d74c44f680d5360a730fb
Mrcha <31120019+mrcha1805@users.noreply.github.com>
2022-10-03 01:22:40 +0700

    support new industry-interface

--- src/app/model/industry-interface.ts
+++ src/app/model/industry-interface.ts
@@ -8,8 +8,7 @@ interface IIndustryData {
   industry_id: number;
   industry_name: string;
   isSelected: boolean | false;
-  industry_description_th?: string;
-  industry_description_en: string;
+  industry_description: string;
   product: Product[];
 }
 
@@ -17,6 +16,5 @@ export interface Product {
   product_id: number;
   product_name: string;
   isSelected: boolean | false;
-  product_description_th: string;
-  product_description_en: string;
+  product_description: string;
 }
